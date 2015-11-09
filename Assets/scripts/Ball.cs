04d4a543fea7c1f492a285b82f52b292d3bff302
Will Field <willjfield@gmail.com>
2015-11-09 13:40:25 -0500

    updated for Unity 5

---
+++ Assets/scripts/Ball.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Vector3 temp = transform.position; // copy to an auxiliary variable...
+		Vector3 stopMo = new Vector3 (0, 0, 0);
+		
+		if (transform.position.x > -226.8) {
+			temp.x = -227.0f; // modify the component you want in the variable...
+			transform.position = temp; // and save the modified value
+			GetComponent<Rigidbody>().velocity = stopMo;
+			GetComponent<Rigidbody>().angularVelocity = stopMo;
+		}
+		if (transform.position.x < -252) {
+
+			temp.x = -251.8f; // modify the component you want in the variable...
+			transform.position = temp; // and save the modified value 
+			GetComponent<Rigidbody>().velocity = stopMo;
+			GetComponent<Rigidbody>().angularVelocity = stopMo;
+
+		}
+		if (transform.position.z > 0) {
+			temp.z = -0.2f; // modify the component you want in the variable...
+			transform.position = temp; // and save the modified value
+			GetComponent<Rigidbody>().velocity = stopMo;
+			GetComponent<Rigidbody>().angularVelocity = stopMo;
+		}
+		if (transform.position.z < -31.5) {
+			temp.z = -31.3f; // modify the component you want in the variable...
+			transform.position = temp; // and save the modified value
+			GetComponent<Rigidbody>().velocity = stopMo;
+			GetComponent<Rigidbody>().angularVelocity = stopMo;
+		}
+		
+	}
+}
---
+++ Assets/scripts/Mallet.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mallet : MonoBehaviour
+{
+		float mousePos = 0.0f;
+		float mousePos2 = 0.0f;
+
+		void Update ()
+		{
+				//Swing Mouse
+				/*
+				mousePos = Input.mousePosition.y;
+				if (Input.GetMouseButton (0)) {						
+						float backswingMouse = mousePos - mousePos2;
+						GetComponent<Rigidbody>().AddForce (transform.forward * backswingMouse * 1.5f);
+						GetComponent<Rigidbody>().useGravity = true;
+				} else {
+						mousePos2 = mousePos;
+				}
+				*/
+				// Swing Keyboard
+				float backswing = Input.GetAxisRaw ("Vertical");
+				GetComponent<Rigidbody>().AddForce (transform.forward * 20 * backswing);
+				GetComponent<Rigidbody>().useGravity = true;	
+				mousePos2 = mousePos;
+
+		}
+}
