8b6e2e0041b920c64f3ea78b1b8e4d44e5b4cb34
Will Field <willjfield@gmail.com>
2016-06-21 22:29:30 +0100

    wickets detect scores

--- Assets/scripts/Ball.cs
+++ Assets/scripts/Ball.cs
@@ -3,14 +3,30 @@ using System.Collections;
 
 public class Ball : MonoBehaviour {
 	public float boundary_W, boundary_E, boundary_S, boundary_N;
+	private int nextWicket;
 	// Use this for initialization
 	void Start () {
 		boundary_W = -254.2f;
 		boundary_E = -226.355f;
 		boundary_S = .2481f;
 		boundary_N = -34.65f;
+		nextWicket = 1;
 	}
-	
+
+	public void setWicket(int wicketNum){
+		nextWicket = wicketNum;
+	}
+
+	public int getWicket(){
+		return nextWicket;
+	}
+
+	public void moveToNextWicket(){
+		nextWicket++;
+
+		Debug.Log (this.gameObject.name+" is going for wicket " + nextWicket);
+	}
+
 	// Update is called once per frame
 	void FixedUpdate () {
 		Vector3 temp = transform.position; // copy to an auxiliary variable...
---
+++ Assets/scripts/ScoreWicket.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreWicket : MonoBehaviour {
+
+	private int[] zDirections = {0,-1,-1,1,1,-1,-1,1,1,-1,-1,1,-1};
+	private int zDirection;
+	private int whichWicket;
+	// Use this for initialization
+	void Start () {
+		string wicket = this.gameObject.name;
+		whichWicket = int.Parse(wicket[wicket.Length - 1].ToString());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider collider){
+
+	}
+
+	void OnTriggerExit(Collider collider){
+		if (collider.GetComponent<Ball>()) {
+			float bV = collider.GetComponent<Rigidbody>().velocity.z;
+			int curBallTarget = collider.GetComponent<Ball> ().getWicket();
+			if(zDirections[curBallTarget]<0 && bV<0 && whichWicket == curBallTarget){
+				collider.GetComponent<Ball> ().moveToNextWicket ();
+			}else if(zDirections[curBallTarget]>0 && bV>0 && whichWicket == curBallTarget){
+				collider.GetComponent<Ball> ().moveToNextWicket ();
+			}
+		}
+		//Debug.Log (collidingBall.nextWicket);
+	}
+}
