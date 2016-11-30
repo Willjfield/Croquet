053c06a34edc2ea041152e9b9254d03285242c7d
Willjfield <willjfield@gmail.com>
2016-11-29 20:33:07 -0500

    Current Game works

--- Assets/scripts/UIManager.cs
+++ Assets/scripts/UIManager.cs
@@ -6,9 +6,20 @@ using System.Collections.Generic;
 public class UIManager : MonoBehaviour {
 
 	int selectedItem; 
+	Transform StatusMenu;
+	public List<Image> ColorButtons;
+	public List<Text> BallText;
 	// Use this for initialization
 	void Start () {
+		StatusMenu = transform.FindChild ("CurrentGameStatus");
+		foreach (Transform child in StatusMenu.transform)
+		{
+			ColorButtons.Add (child.GetComponent<Image> ());
+			Transform[] grandchildren = child.GetComponentsInChildren<Transform> ();
+			BallText.Add (grandchildren[1].GetComponent<Text>());
+		}
 
+		toggleGameStatusComponents (false);
 	}
 
 	// Update is called once per frame
@@ -20,23 +31,35 @@ public class UIManager : MonoBehaviour {
 		selectedItem = GameObject.Find("Dropdown").GetComponent<Dropdown>().value;
 		switch (selectedItem) {
 		case 0:
-			transform.FindChild ("CurrentGameStatus").gameObject.SetActive (false);
+			toggleGameStatusComponents(false);
 			break;
 		case 1:
-			transform.FindChild ("CurrentGameStatus").gameObject.SetActive (true);
+			toggleGameStatusComponents(true);
+
 			break;
 		case 2:
 			Application.LoadLevel("Main_Scene");
 			break;
 		case 3:
-			transform.FindChild ("CurrentGameStatus").gameObject.SetActive (false);
+			toggleGameStatusComponents(false);
 			break;
 		case 4:
-			transform.FindChild ("CurrentGameStatus").gameObject.SetActive (false);
+			toggleGameStatusComponents(false);
 			break;
 		case 5:
 			Application.Quit();
 			break;
+		}
+	}
+
+	private void toggleGameStatusComponents(bool state){
+		foreach (Image img in ColorButtons)
+		{
+			img.enabled = state;
+		}
+		foreach (Text txt in BallText)
+		{
+			txt.enabled = state;
 		}
 	}
 }
