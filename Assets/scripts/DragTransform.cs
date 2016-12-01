467fd1a846b19fe56e84281f3f5fad1ad25faa36
Willjfield <willjfield@gmail.com>
2016-11-30 23:21:18 -0500

    new icon and raycasting ball placement

--- Assets/scripts/DragTransform.cs
+++ Assets/scripts/DragTransform.cs
@@ -17,17 +17,27 @@ class DragTransform : MonoBehaviour
 
 	void onDrag(){
 		
-		Vector3 currentScreenPoint;
-
-		currentScreenPoint = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
-
-		Vector3 currentPos = Camera.main.ScreenToWorldPoint (currentScreenPoint);
-
-		transform.position = currentPos;
-		if (transform.position.y < -0.08268002f) {
-			float X = transform.position.x; 
-			float Z = transform.position.z;
-			transform.position = new Vector3 (X, -0.08268002f, Z);		
+//		Vector3 currentScreenPoint;
+//
+//		currentScreenPoint = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+//
+//		Vector3 currentPos = Camera.main.ScreenToWorldPoint (currentScreenPoint);
+//
+//		transform.position = currentPos;
+//		if (transform.position.y < -0.08268002f) {
+//			float X = transform.position.x; 
+//			float Z = transform.position.z;
+//			transform.position = new Vector3 (X, -0.08268002f, Z);		
+//		}
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		// create a plane at 0,0,0 whose normal points to +Y:
+		Plane hPlane = new Plane(Vector3.up, new Vector3(0,-0.08268008f,0));
+		// Plane.Raycast stores the distance from ray.origin to the hit point in this variable:
+		float distance = 0; 
+		// if the ray hits the plane...
+		if (hPlane.Raycast(ray, out distance)){
+			// get the hit point:
+			transform.position = ray.GetPoint(distance);
 		}
 		ballFreeze ();
 		
